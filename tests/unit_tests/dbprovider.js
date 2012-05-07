d193dc23432f59e1fc3dcf41dba52b75b9866886
Denis Rechkunov <denis.rechkunov@gmail.com>
2012-05-07 22:07:11 +0700

    Added lost test folder.

---
+++ tests/unit_tests/dbprovider.js
@@ -0,0 +1,78 @@
+/* 
+ * neutrino
+ *
+ * Copyright (c) 2012 Denis Rechkunov and project contributors.
+ *
+ * neutrino's license follows:
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, 
+ * publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * This license applies to all parts of neutrino that are not externally
+ * maintained libraries.
+ */
+var neutrino = require('../../index.js'),
+    randomValue = Math.random(),
+    config = new neutrino.core.Config(),
+    dbProvider = new neutrino.io.DbProvider(config);
+
+exports["Create collection and insert to database"] = function (test) {
+    test.expect(2);
+    dbProvider.getCollection('testNeutrino', function (collection) {
+        collection.insert({hello:'world', test:randomValue}, function (error, object) {
+            test.ifError(error);
+            test.deepEqual(object[0].test, randomValue);
+            test.done();
+        });
+    });
+};
+
+exports["Find in collection"] = function (test) {
+    test.expect(3);
+    dbProvider.getCollection('testNeutrino', function (collection) {
+        collection.findOne({test:randomValue}, function (error, object) {
+            test.ifError(error);
+            test.deepEqual(object.hello, 'world');
+            test.deepEqual(object.test, randomValue);
+            test.done();
+        });
+    });
+};
+
+exports['Remove from collection'] = function (test) {
+    test.expect(2);
+    dbProvider.getCollection('testNeutrino', function (collection) {
+        collection.remove({test:randomValue}, {safe:true}, function (error, removedCount) {
+            test.ifError(error);
+            test.deepEqual(removedCount, 1, 'Item to remove not found');
+            test.done();
+        });
+
+    });
+};
+
+exports['Drop collection from database'] = function (test) {
+    test.expect(1);
+    dbProvider.getCollection('testNeutrino', function (collection) {
+        collection.drop(function (error) {
+            test.ifError(error);
+            test.done();
+        })
+    });
+};
---
+++ tests/unit_tests/sessionmanager.js
@@ -0,0 +1,121 @@
+/*
+ * neutrino
+ *
+ * Copyright (c) 2012 Denis Rechkunov and project contributors.
+ *
+ * neutrino's license follows:
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This license applies to all parts of neutrino that are not externally
+ * maintained libraries.
+ */
+
+var neutrino = require('../../index.js'),
+    config = new neutrino.core.Config({
+        sessions:{
+            expiredTimeout:100,
+            checkExpiredInterval:200
+        }
+    }),
+    randomValue = Math.random(),
+    createdSessionId,
+    sessionManager1 = new neutrino.security.SessionManager(config),
+    sessionManager2 = new neutrino.security.SessionManager(config);
+
+exports['Create session'] = function (test) {
+    test.expect(3);
+    var toAdd = {
+        user:'test',
+        key:randomValue
+    };
+    sessionManager1.create(toAdd, function (error, object, sessionId) {
+        test.ifError(error);
+        test.deepEqual(object.user, 'test');
+        test.deepEqual(object.key, randomValue);
+        createdSessionId = sessionId;
+        test.done();
+    });
+};
+
+exports['Get session'] = function (test) {
+    test.expect(3);
+    sessionManager2.$(createdSessionId, function (error, object) {
+        test.ifError(error);
+        test.deepEqual(object.user, 'test');
+        test.deepEqual(object.key, randomValue);
+        test.done();
+    });
+};
+
+exports['Set value to session'] = function (test) {
+    test.expect(6);
+    sessionManager1.$(createdSessionId, function (error, object) {
+        test.ifError(error);
+        test.deepEqual(object.user, 'test2');
+        test.deepEqual(object.key, randomValue);
+        sessionManager2.$(createdSessionId, function (error, object) {
+            test.ifError(error);
+            test.deepEqual(object.user, 'test2');
+            test.deepEqual(object.key, randomValue);
+            test.done();
+        });
+    }, {user:'test2'});
+};
+
+exports['Remove session'] = function (test) {
+    test.expect(3);
+    sessionManager1.remove(createdSessionId, function (error) {
+        test.ifError(error);
+        sessionManager2.get(createdSessionId, function (error, object) {
+            test.ifError(error);
+            test.deepEqual(object, null);
+            test.done();
+        });
+    });
+};
+
+exports['Expired session get'] = function (test) {
+
+    test.expect(5);
+
+    var toAdd = {
+            user:'test2',
+            key:randomValue
+        },
+        expiredSessionId;
+
+
+    sessionManager1.create(toAdd, function (error, object, sessionId) {
+        test.ifError(error);
+        test.deepEqual(object.user, 'test2');
+        test.deepEqual(object.key, randomValue);
+        expiredSessionId = sessionId;
+    });
+
+    setTimeout(function () {
+        sessionManager2.$(expiredSessionId, function (error, object) {
+            test.ifError(error);
+            test.deepEqual(object, null);
+            test.done();
+        });
+
+    }, config.$('sessions').checkExpiredInterval * 2 + 100);
+};
